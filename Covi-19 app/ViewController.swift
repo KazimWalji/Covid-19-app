85a0874a2f0a80b3ca7f3093d27a5e37bb9e57e0
Kazim Walji <kwalji@bsd220.org>
2020-11-08 13:14:04 -0600

    Initial Commit

---
+++ Covi-19 app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covi-19 app
+//
+//  Created by Kazim Walji on 11/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
